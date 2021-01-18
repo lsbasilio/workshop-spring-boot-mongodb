7b7f99ed944e4329d2f623fd18f8cd4694c2c491
Leandro Basilio <leandro_sanzi@yahoo.com.br>
2021-01-18 17:42:43 -0300

    Referenciando os posts do usuário

--- src/main/java/com/leandro/workshopmongo/domain/User.java
+++ src/main/java/com/leandro/workshopmongo/domain/User.java
@@ -1,11 +1,14 @@
 package com.leandro.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document // Mongodb
+@Document(collection = "user") // Mongodb
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // lazy: Só mostra os Posts quando solicitado
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {		
 	}
@@ -48,6 +54,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -73,7 +87,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
