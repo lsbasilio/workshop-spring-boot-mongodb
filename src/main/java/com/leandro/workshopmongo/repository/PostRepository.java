ac98f5475e9df47988244793212dde55da91227b
Leandro Basilio <leandro_sanzi@yahoo.com.br>
2021-01-20 16:20:04 -0300

    Consulta simples com query methods

--- src/main/java/com/leandro/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leandro/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.leandro.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.leandro.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	
+	// Query Methods
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
--- src/main/java/com/leandro/workshopmongo/resources/PostResource.java
+++ src/main/java/com/leandro/workshopmongo/resources/PostResource.java
@@ -1,21 +1,17 @@
 package com.leandro.workshopmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leandro.workshopmongo.domain.Post;
-import com.leandro.workshopmongo.domain.User;
-import com.leandro.workshopmongo.dto.UserDTO;
+import com.leandro.workshopmongo.resources.util.URL;
 import com.leandro.workshopmongo.services.PostService;
 
 @RestController
@@ -29,5 +25,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);		
 		return ResponseEntity.ok().body((obj));
+	}
+	
+	// Query Methods
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET) // ou @GetMapping
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);		
+		List<Post> list = service.findByTitle(text);		
+		return ResponseEntity.ok().body((list));
 	}
 }
